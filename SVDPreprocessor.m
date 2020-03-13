1df17cb348dd14a821a3ed3f38b291250b3cc909
Will Thomas <44074503+WillTUW@users.noreply.github.com>
2020-03-13 15:10:52 -0700

    Update SVDPreprocessor.m

--- SVDPreprocessor.m
+++ SVDPreprocessor.m
@@ -1,12 +1,10 @@
 function imageData = SVDPreprocessor(imageData, featureVectorLength)
     for i = 1:length(imageData)
-        %tempMat = reshape(imageData(:, i), [28, 28]);
-        %[U, S, V] = svd(tempMat, 'econ');
-        %approx = U(:, 1:featureVectorLength) * S(1:featureVectorLength, 1:featureVectorLength) * V(:, 1:featureVectorLength)';
-        %approx = reshape(approx.',1,[]);
-        tempP = imageData(:,i);
-        matGaussian = (1/16)*[1 2 1; 2 4 2; 1 2 1];
+        tempMat = reshape(imageData(:, i), [28, 28]);
+        [U, S, V] = svd(tempMat, 'econ');
+        approx = U(:, 1:featureVectorLength) * S(1:featureVectorLength, 1:featureVectorLength) * V(:, 1:featureVectorLength)';
+        approx = reshape(approx.',1,[]);
         tempMat = conv2(tempP, matGaussian, 'same');
-        imageData(:, i) = tempMat;%approx;
+        imageData(:, i) = approx;
     end
 end
